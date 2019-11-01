201245a6baccba52d0b92ce2c1527f52a0a16b9a
Antonio Sanchez <antonio@kokoen.net>
2019-11-01 18:01:07 +0100

    Some minor refactoring

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
+const (
+	AWS_S3_REGION = ""
+	AWS_S3_BUCKET = ""
+)
+
 var sess = connectAWS()
 
 func connectAWS() *session.Session {
@@ -24,11 +29,6 @@ func connectAWS() *session.Session {
 	}
 	return sess
 }
-
-const (
-	AWS_S3_REGION = ""
-	AWS_S3_BUCKET = ""
-)
 
 func main() {
 
